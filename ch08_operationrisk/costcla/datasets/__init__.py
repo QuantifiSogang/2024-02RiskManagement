8479634ddba8bc2d234a0225b022e83d8a4a20b4
kumb222 <kumb222@gmail.com>
2024-12-18 15:22:46 +0900

    ch08 업로드

---
+++ ch08_operationrisk/costcla/datasets/__init__.py
@@ -0,0 +1,15 @@
+"""
+The :mod:`costcla.datasets` module includes utilities to load datasets,
+including methods to load and fetch popular reference datasets. It also
+features some artificial data generators.
+"""
+
+from .base import load_bankmarketing
+from .base import load_creditscoring1
+from .base import load_creditscoring2
+from .base import load_churn
+
+__all__ = ['load_bankmarketing',
+           'load_creditscoring1',
+           'load_creditscoring2',
+           'load_churn']
---
+++ ch08_operationrisk/costcla/probcal/__init__.py
@@ -0,0 +1,7 @@
+"""
+The :mod:`costcla.probcal` module includes methods for probability calibration
+"""
+
+from .probcal import ROCConvexHull
+
+__all__ = ['ROCConvexHull',]
